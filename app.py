8f03793560c35e2d6a6912bfec013af8a80f4571
Naveenkrishna <naveenkrishnank02@gmail.com>
2026-01-17 11:50:01 +0530

    12

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+def hello():
+    return 'Hello from Flask! Updated: 2026-01-17'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
